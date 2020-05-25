e26fd1e23a85063cdd5afe1652bbe52bf7acc8be
Fabian Zeiher <fzeiher@gmail.com>
2020-05-25 14:36:55 +0200

    add multiple cats

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
+var initialCats = [{
+        "name": "Flauschi",
+        "imgSrc": "img/4154543904_6e2428c421_z.jpg",
+        "clickCount": 0,
+        "nicknames": ['Flachmann', 'Flaunders']
+    },
+    {
+        "name": "Wuschel",
+        "imgSrc": "img/22252709_010df3379e_z.jpg",
+        "clickCount": 0,
+        "nicknames": ['Mister W', 'Wall_E']
+    },
+    {
+        "name": "Tobbsen",
+        "imgSrc": "img/434164568_fea0ad4013_z.jpg",
+        "clickCount": 0,
+        "nicknames": ['Tobbsen', 'Tabbers', 'Tabberman']
+    },
+    {
+        "name": "Gustaf",
+        "imgSrc": "img/1413379559_412a540d29_z.jpg",
+        "clickCount": 0,
+        "nicknames": ['Hubert', 'Olaf', 'Jens']
+    },
+    {
+        "name": "Quaschning",
+        "imgSrc": "img/9648464288_2516b35537_z.jpg",
+        "clickCount": 0,
+        "nicknames": ['Professor']
+    }
+];
+
 var Cat = function (data) {
     this.clickCount = ko.observable(data.clickCount);
     this.name = ko.observable(data.name);
@@ -12,16 +44,18 @@ var Cat = function (data) {
 }
 
 var ViewModel = function () {
+    self = this;
 
-    this.currentCat = ko.observable(new Cat({
-        clickCount: 0,
-        name: 'Tabby',
-        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-        nicknames: ['Tobbsen', 'Tabbers', 'Tabberman']
-    }));
+    this.cats = ko.observableArray();
+
+    initialCats.forEach(function (catItem) {
+        self.cats.push(new Cat(catItem));
+    })
+
+    this.currentCat = ko.observable(self.cats()[0]);
 
     this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     }
 }
 
