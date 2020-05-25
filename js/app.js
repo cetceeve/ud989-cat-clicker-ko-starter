e5b327895083c389589ea8e3acbbc053e8940949
Fabian Zeiher <fzeiher@gmail.com>
2020-05-25 11:20:59 +0200

    display cat nicknames

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 var ViewModel = function () {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg')
+    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.nicknames = ko.observableArray(['Tobbsen', 'Tabbers', 'Tabberman']);
 
     this.level = ko.computed(function () {
         if (this.clickCount() > 19) return 'Teen';
