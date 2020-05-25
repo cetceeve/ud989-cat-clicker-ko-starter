873c5e3ae513bf4f61cb99be305d3e885a9989fc
Fabian Zeiher <fzeiher@gmail.com>
2020-05-25 11:15:00 +0200

    add levels

--- js/app.js
+++ js/app.js
@@ -3,6 +3,12 @@ var ViewModel = function () {
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg')
 
+    this.level = ko.computed(function () {
+        if (this.clickCount() > 19) return 'Teen';
+        if (this.clickCount() > 9) return 'Newborn';
+        return 'Infant';
+    }, this);
+
     this.incrementCounter = function () {
         this.clickCount(this.clickCount() + 1);
     }
